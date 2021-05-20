045027cd9d8fd081f8fab162cdd0c68ea462fa32
piyushyeolekar <piyushyeolekar30@gmail.com>
2021-05-21 03:02:10 +0530

    Added the controller, service and DAO layer. Added some comments. Added dependencies and configurations.

--- src/main/java/com/user/entity/User.java
+++ src/main/java/com/user/entity/User.java
@@ -1,8 +1,75 @@
 package com.user.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 	
-	private long userId;
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
 	private String name;
 	private String phone;
+	
+	List<Contact> contacts = new ArrayList<>();
+
+	public User(int userId, String name, String phone, List<Contact> contacts) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.phone = phone;
+		this.contacts = contacts;
+	}
+
+	public User(int userId, String name, String phone) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public User() {
+		super();
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+	
 }
